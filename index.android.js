5851407d2b355008f06882588de98f6ada7c1307
Javier Carrillo <stringparser@gmail.com>
2016-08-01 23:16:14 +0200

    dev: found a way to fetch data from instagram easely

--- index.android.js
+++ index.android.js
@@ -1,9 +1,9 @@
 
 // module imports
 let React = require('react');
-let { Text, View, AppRegistry } = require('react-native');
+let { AppRegistry } = require('react-native');
 
-let styles = require('./lib/styles');
+// app startup components
 let Loading = require('./lib/Loading');
 let ImageCarousel = require('./lib/ImageCarousel');
 
@@ -18,22 +18,26 @@ let App = React.createClass({
     this.fetchImages();
   },
   fetchImages: function () {
+    var url = 'https://www.instagram.com/berlinninja/media/';
     var self = this;
-    fetch('http://unsplash.it/list').then(function (response) {
-      return response.json();
-    }).then(function (jsonData) {
-      console.log(jsonData);
-      self.setState({ images: jsonData.slice(0, 5), isLoading: false });
+    fetch(url).then(function (res) {
+      return res.json();
+    }).then(function (data) {
+      self.setState({
+        images: data.items.map(function (item) {
+          return ({
+            url: item.images.standard_resolution.url,
+            date: item.caption.created_time,
+            label: item.caption.text.match(/[^\.]+/).pop(),
+            author: item.user.username,
+            caption: item.caption.text
+          });
+        }),
+        isLoading: false
+      });
     }).catch(function (error) {
       console.log('Fetch error ' + error);
     });
-  },
-  renderResultsScreen: function () {
-    return (
-      <View style={styles.container}>
-        <Text>Results</Text>
-      </View>
-    );
   },
   render: function() {
     if (this.state.isLoading) {
