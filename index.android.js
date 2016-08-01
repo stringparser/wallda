bf805c7ef1b933a4356f0028016398b05bbaf758
Javier Carrillo <stringparser@gmail.com>
2016-08-01 21:40:13 +0200

    dev: split app into components already

--- index.android.js
+++ index.android.js
@@ -1,50 +1,47 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
 
-import React from 'react';
-import * as ReactNative from 'react-native';
+// module imports
+let React = require('react');
+let { Text, View, AppRegistry } = require('react-native');
 
-let Text = ReactNative.Text;
-let View = ReactNative.View;
-let Image = ReactNative.Image;
-let StyleSheet = ReactNative.StyleSheet;
-let AppRegistry = ReactNative.AppRegistry;
+let styles = require('./lib/styles');
+let Loading = require('./lib/Loading');
+let ImageCarousel = require('./lib/ImageCarousel');
 
-class wallda extends React.Component {
-  render() {
+let App = React.createClass({
+  getInitialState: function () {
+    return ({
+      images: [],
+      isLoading: true
+    });
+  },
+  componentDidMount: function () {
+    this.fetchImages();
+  },
+  fetchImages: function () {
+    var self = this;
+    fetch('http://unsplash.it/list').then(function (response) {
+      return response.json();
+    }).then(function (jsonData) {
+      console.log(jsonData);
+      self.setState({ images: jsonData.slice(0, 5), isLoading: false });
+    }).catch(function (error) {
+      console.log('Fetch error ' + error);
+    });
+  },
+  renderResultsScreen: function () {
     return (
       <View style={styles.container}>
-        <Image source={{uri: 'http://generator-meme.com/memes/successful-mexican_55a04b777e3626818.jpg'}}
-       style={{width: 400, height: 400}} />
-       <Text style={styles.welcome}>
-         I don't get it! :D
-       </Text>
+        <Text>Results</Text>
       </View>
     );
-  }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF'
   },
-  welcome: {
-    color: '#000',
-    margin: 10,
-    fontSize: 20,
-    textAlign: 'center'
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5
+  render: function() {
+    if (this.state.isLoading) {
+      return <Loading/>;
+    } else {
+      return <ImageCarousel {...this.state}/>;
+    }
   }
 });
 
-AppRegistry.registerComponent('wallda', () => wallda);
+AppRegistry.registerComponent('wallda', () => App);
